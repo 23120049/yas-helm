b59434ed3def4f40a60e67ada2fb6c4a5b107eb2
cgb <huyen211105@gmail.com>
2026-06-29 21:38:50 +0700

    edit redirect urls

--- deploy/setup-keycloak.sh
+++ deploy/setup-keycloak.sh
@@ -4,11 +4,13 @@ set -x
 #Read configuration value from cluster-config.yaml file
 read -rd '' DOMAIN POSTGRESQL_USERNAME POSTGRESQL_PASSWORD \
 BOOTSTRAP_ADMIN_USERNAME BOOTSTRAP_ADMIN_PASSWORD \
-KEYCLOAK_BACKOFFICE_REDIRECT_URL KEYCLOAK_STOREFRONT_REDIRECT_URL \
+KEYCLOAK_BACKOFFICE_REDIRECT_URLS KEYCLOAK_STOREFRONT_REDIRECT_URLS KEYCLOAK_API_REDIRECT_URLS \
 < <(yq -r '.domain,
   .postgresql.username, .postgresql.password,
   .keycloak.bootstrapAdmin.username, .keycloak.bootstrapAdmin.password,
-  .keycloak.backofficeRedirectUrl, .keycloak.storefrontRedirectUrl' ./cluster-config.yaml)
+  (.keycloak.backofficeRedirectUrls | to_json),
+  (.keycloak.storefrontRedirectUrls | to_json),
+  (.keycloak.apiRedirectUrls | to_json)' ./cluster-config.yaml)
 
 #Install CRD keycloak
 kubectl create namespace keycloak
@@ -24,5 +26,6 @@ helm upgrade --install keycloak ./keycloak/keycloak \
 --set postgresql.password="$POSTGRESQL_PASSWORD" \
 --set bootstrapAdmin.username="$BOOTSTRAP_ADMIN_USERNAME" \
 --set bootstrapAdmin.password="$BOOTSTRAP_ADMIN_PASSWORD" \
---set backofficeRedirectUrl="$KEYCLOAK_BACKOFFICE_REDIRECT_URL" \
---set storefrontRedirectUrl="$KEYCLOAK_STOREFRONT_REDIRECT_URL"
+--set-json backofficeRedirectUrls="$KEYCLOAK_BACKOFFICE_REDIRECT_URLS" \
+--set-json storefrontRedirectUrls="$KEYCLOAK_STOREFRONT_REDIRECT_URLS" \
+--set-json apiRedirectUrls="$KEYCLOAK_API_REDIRECT_URLS"
